9ae3f7a4529e7d62ac412c257201aacb6d39351b
Dominic Tölle <dominic.tolle@gmail.com>
2022-11-22 15:59:41 +0100

    Add eslint and prettier

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -1,4 +1,8 @@
 module.exports = {
     root: true,
-    extends: ['@react-native-community', 'airbnb-typescript', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
+    plugins: ["import"],
+    extends: ['@react-native-community', 'airbnb-typescript', 'prettier'],
+    parserOptions: {
+        project: "./tsconfig.json"
+    },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import { registerRootComponent } from 'expo';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
 
-function App() {
-  return (
-    <>
-      <StatusBar style="auto" />
-      <SafeAreaView style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-      </SafeAreaView>
-    </>
-  );
-}
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
+
+function App() {
+    return (
+        <>
+            <StatusBar style="auto" />
+            <SafeAreaView style={styles.container}>
+                <Text>Open up App.tsx to start working on your app!</Text>
+            </SafeAreaView>
+        </>
+    );
+}
 
 export default registerRootComponent(App);
